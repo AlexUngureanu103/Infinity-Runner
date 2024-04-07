686a9fcb6684a2eb54eb4c75cd3322790e062893
AlexDaniel02 <alexdaniel238@yahoo.ro>
2024-04-07 21:44:37 +0300

    added tall obstacles

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -2,73 +2,87 @@ using UnityEngine;
 
 public class GroundTile : MonoBehaviour
 {
-    private GroundSpawner _GroundSpawner;
+	private GroundSpawner _GroundSpawner;
 
-    [SerializeField]
-    private GameObject _ObstaclePrefab;
+	[SerializeField]
+	private GameObject _ObstaclePrefab;
 
-    [SerializeField]
-    private GameObject _CoinPrefab;
+	[SerializeField]
+	private GameObject _TallObstaclePrefab;
 
-    void Start()
-    {
-        _GroundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
-        if (transform.position.z >= 20)
-        {
-            SpawnObstacle();
-        }
-        SpawnCoin();
-    }
+	[SerializeField]
+	private float _TallObstacleChance = 0.3f;
 
-    void OnTriggerExit(Collider other)
-    {
-        _GroundSpawner.SpawnTile();
-        Destroy(gameObject, 2);
-    }
+	[SerializeField]
+	private GameObject _CoinPrefab;
 
-    void Update()
-    {
+	void Start()
+	{
+		_GroundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+		if (transform.position.z >= 20)
+		{
+			SpawnObstacle();
+		}
+		SpawnCoin();
+	}
 
-    }
+	void OnTriggerExit(Collider other)
+	{
+		_GroundSpawner.SpawnTile();
+		Destroy(gameObject, 2);
+	}
 
-    public void SpawnObstacle()
-    {
-        int obstacleSpawnIndex = Random.Range(0, 3);
-        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+	void Update()
+	{
 
-        if (!GetComponent<Collider>().bounds.Contains(spawnPoint.position))
-        {
-            return;
-        }
+	}
 
-        Instantiate(_ObstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
-    }
+	public void SpawnObstacle()
+	{
+		GameObject obstacleToSpawn = _ObstaclePrefab;
+		float random = Random.Range(0f, 1f);
 
-    public void SpawnCoin()
-    {
-        int coinsToSpawn = Random.Range(0, 5);
-        for (int i = 0; i < coinsToSpawn; i++)
-        {
-            var tempCoin = Instantiate(_CoinPrefab, transform);
-            tempCoin.transform.position = GetRandomPointCollider(GetComponent<Collider>());
-        }
-    }
+		if (random < _TallObstacleChance)
+		{
+			obstacleToSpawn = _TallObstaclePrefab;
+		}
 
-    Vector3 GetRandomPointCollider(Collider collider)
-    {
-        Vector3 point = new Vector3(
-            Random.Range(collider.bounds.min.x, collider.bounds.max.x),
-            Random.Range(collider.bounds.min.y, collider.bounds.max.y),
-            Random.Range(collider.bounds.min.z, collider.bounds.max.z)
-            );
+		int obstacleSpawnIndex = Random.Range(0, 3);
+		Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+
+		if (!GetComponent<Collider>().bounds.Contains(spawnPoint.position))
+		{
+			return;
+		}
+
+		Instantiate(obstacleToSpawn, spawnPoint.position, Quaternion.identity, transform);
+	}
+
+	public void SpawnCoin()
+	{
+		int coinsToSpawn = Random.Range(0, 5);
+		for (int i = 0; i < coinsToSpawn; i++)
+		{
+			var tempCoin = Instantiate(_CoinPrefab, transform);
+			tempCoin.transform.position = GetRandomPointCollider(GetComponent<Collider>());
+		}
+	}
+
+	Vector3 GetRandomPointCollider(Collider collider)
+	{
+		Vector3 point = new Vector3(
+			Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+			Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+			Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+			);
 
 
-        if (!collider.bounds.Contains(point))
-        {
-            point = GetRandomPointCollider(collider);
-        }
+		if (!collider.bounds.Contains(point))
+		{
+			point = GetRandomPointCollider(collider);
+		}
 
-        point.y = 1;
-        return point;
-    }
+		point.y = 1;
+		return point;
+	}
 }
